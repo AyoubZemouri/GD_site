3ceef9aba5fcfc921d595169631d7a2a9441c557
Ayoub <zemouri.ayoub@gmail.com>
2021-10-07 09:13:38 -0400

    corrections

--- src/components/CountComp.js
+++ src/components/CountComp.js
@@ -4,9 +4,9 @@ import CountUp from 'react-countup';
 export const CountComp = () => {
     return (
         <div className="countup">
-            <section className="counter red"><CountUp end={200} duration={3} delay={2} /></section>
+            <section className="counter red"><CountUp end={2000} duration={3} delay={2} /></section>
             <section className="counter blue"><CountUp end={1256000} duration={3} separator=" " delay={2} /></section>
-            <section className="counter yellow"><CountUp end={20} duration={3} delay={2} /></section>
+            <section className="counter yellow"><CountUp end={40} duration={3} delay={2} /></section>
             
             <section className="description">CLIENTS SATISFAITS</section>
             <section className="description">FEUILLES IMPRIMÉES</section>
